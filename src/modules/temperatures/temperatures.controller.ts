7705d152387f5bac466868049ca45130e4f97254
Manuel Selch <manuel@selch.de>
2025-05-08 18:56:54 +0200

    example data

--- src/modules/temperatures/temperatures.controller.ts
+++ src/modules/temperatures/temperatures.controller.ts
@@ -1,5 +1,6 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { TemperatureRepository } from './infra/temperature.repository';
+import { CreateTemperatureDto } from './api/create-temperature.dto';
 
 @Controller('temperatures')
 export class TemperaturesController {
@@ -13,12 +14,21 @@ export class TemperaturesController {
     }
 
     @Post("/")
-    async create(@Body() createTemperature: Temperature) {
+    async create(@Body() createTemperature: CreateTemperatureDto) {
         await this.temperatureRepo.create(createTemperature);
 
         return {
             diff: 0,
-            timestamp: "00:00"
+            timestamp: this.getTimestamp()
         }
+    }
+
+    private getTimestamp() {
+        const now = new Date();
+        const hours = now.getHours().toString().padStart(2, '0');
+        const minutes = now.getMinutes().toString().padStart(2, '0');
+
+        const timestamp = `${hours}:${minutes}`;
+        return timestamp;
     }
 }
