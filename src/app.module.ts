48f6848335961d26e1e035974c4c0a862b58c935
Manuel Selch <manuel@selch.de>
2025-05-08 19:55:45 +0200

    fix error

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { TemperaturesModule } from './modules/temperatures/temperatures.module';
-import '@mantine/charts/styles.css';
 
 @Module({
   imports: [TemperaturesModule],
